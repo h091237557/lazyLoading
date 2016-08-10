759ba767f37646a8a1641e7648bf3a2c1831237a
mark <h091237557@gmail.com>
2016-08-10 22:51:16 +0800

    Add:add scroll position

--- lazy.js
+++ lazy.js
@@ -13,6 +13,29 @@
 		}
 	}
 
+	function lazy(){
+		if(!elements.length) return;
+		var innerHeigh = this.getPosition.ViewPort(),
+				ellength = el.length,
+				index = 0;
+
+				for (var i=0;i<ellength;i++){
+					index = this.getPosition.ElementViewTop(elements[i]);
+					if(index - this.getPosition.ScrollTop() < innerHeigh){
+						elements[i].src = elements[i].getAttribute("lazy_src");
+						delete elements[i];
+					}	
+				}
+	}
+
+	function slowLoad (){
+		window.onscroll = window.onload = function(){
+			setTimeout(function(){
+				
+			},1000);
+		}
+	
+	}
 	var getPosition: {
 		ViewPort: function() {
 			var height = 0;
@@ -31,9 +54,25 @@
 				elementScrollTop = document.body.scrollTop;
 			}
 			return elementScrollTop;
+		},
+		ElementViewTop: function(el) {
+			var actualTop = 0;
+			current = 0;
+
+			if (el) {
+				actualTop = el.offsetTop,
+				current = el.offsetParent;
+				while (current != null) {
+					actualTop += current.offsetTop;
+					current = current.offsetParent;
+				}
+				return actualTop - this.ScrollTop();
+
+			}
 		}
 
 	}
+
 
 })();
 
