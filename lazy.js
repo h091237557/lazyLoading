4fb8926ead54dc9f9a71cca0f911c8de2692b23d
mark <h091237557@gmail.com>
2016-08-13 20:47:16 +0800

    Moidfy : modify lazy.js

--- lazy.js
+++ lazy.js
@@ -1,78 +1,83 @@
 (function() {
 	var elements = [];
-
-	function init() {
-		var el = document.getElementByTagName('img'),
-		ellenght = el.length;
-
-		for (var i = 0; i < ellength; i++) {
-			if (typeof(el[i].getAttribute("lazy_src"))) {
-				elements.push(el[i];)
-			}
-
-		}
-	}
-
-	function lazy(){
-		if(!elements.length) return;
-		var innerHeigh = this.getPosition.ViewPort(),
-				ellength = el.length,
-				index = 0;
-
-				for (var i=0;i<ellength;i++){
-					index = this.getPosition.ElementViewTop(elements[i]);
-					if(index - this.getPosition.ScrollTop() < innerHeigh){
-						elements[i].src = elements[i].getAttribute("lazy_src");
-						delete elements[i];
-					}	
-				}
-	}
-
-	function slowLoad (){
-		window.onscroll = window.onload = function(){
-			setTimeout(function(){
-				
-			},1000);
-		}
-	
-	}
-	var getPosition: {
-		ViewPort: function() {
-			var height = 0;
-			if (document.compatModel == "BackCompat") {
-				height = document.body.clientHeight;
-			} else {
-				heigth = document.documentElement.clientHeight;
-			}
-			return height;
+	var lazyLoad = {
+		init:function(){
+			this.setLayzimg();
+			this.lazy();
+			this.slowLoad();
 		},
-		ScrollTop: function() {
-			var elementScrollTop = 0;
-			if (document.compatModel == "BackCompat") {
-				elementScrollTop = document.documentElement.scrollTop;
-			} else {
-				elementScrollTop = document.body.scrollTop;
-			}
-			return elementScrollTop;
-		},
-		ElementViewTop: function(el) {
-			var actualTop = 0;
-			current = 0;
-
-			if (el) {
-				actualTop = el.offsetTop,
-				current = el.offsetParent;
-				while (current != null) {
-					actualTop += current.offsetTop;
-					current = current.offsetParent;
+		setLayzimg: function() {
+			var el = document.getElementsByTagName('img'),
+			ellength = el.length;
+			for (var i = 0; i < ellength; i++) {
+				if (typeof(el[i].getAttribute("lazy_src"))) {
+					elements.push(el[i]);
 				}
-				return actualTop - this.ScrollTop();
 
 			}
+		},
+		lazy: function() {
+			if (!elements.length) return;
+			var innerHeigh = this.getPosition.ViewPort(),
+			ellength = elements.length,
+			index = 0;
+
+			for (var i = 0; i < ellength; i++) {
+				index = this.getPosition.ElementViewTop(elements[i]);
+				if (index - this.getPosition.ScrollTop() < innerHeigh) {
+					elements[i].src = elements[i].getAttribute("lazy_src");
+					delete elements[i];
+				}
+			}
+		},
+		slowLoad: function() {
+			var that = this;
+			window.onscroll = window.onload = function() {
+				setTimeout(function() {
+					that.lazy();
+				},
+				1000);
+			}
+
+		},
+		getPosition: {
+
+			ViewPort: function() {
+				var height = 0;
+				if (document.compatModel == "BackCompat") {
+					height = document.body.clientHeight;
+				} else {
+					heigth = document.documentElement.clientHeight;
+				}
+				return height;
+			},
+			ScrollTop: function() {
+				var elementScrollTop = 0;
+				if (document.compatModel == "BackCompat") {
+					elementScrollTop = document.documentElement.scrollTop;
+				} else {
+					elementScrollTop = document.body.scrollTop;
+				}
+				return elementScrollTop;
+			},
+			ElementViewTop: function(el) {
+				var actualTop = 0;
+				current = 0;
+
+				if (el) {
+					actualTop = el.offsetTop,
+					current = el.offsetParent;
+					while (current != null) {
+						actualTop += current.offsetTop;
+						current = current.offsetParent;
+					}
+					return actualTop - this.ScrollTop();
+
+				}
+			}
+
 		}
-
 	}
-
-
+	lazyLoad.init();	
 })();
 
